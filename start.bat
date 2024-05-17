d817f5a2f20188ed33470b8ebe61f5b6ff027a48
64 <337667699@qq.com>
2024-05-17 10:56:55 +0800

    start.bat

--- start.bat
+++ start.bat
@@ -10,6 +10,10 @@ copy j-eureka/build-jenkins/Dockerfile j-eureka/target/Dockerfile
 cd j-eureka/target
 docker build -t j-eureka-2.7.4 . && docker run -p 8088:8088 -v D:\Docker\Users\logs:/logs --name eurekadocker.2.7.4 -d --privileged --network my-network j-eureka-2.7.4
 
+cd ../../j-config-server/target
+copy j-eureka/build-jenkins/Dockerfile j-config/target/Dockerfile
+docker build -t j-config . && docker run -p 8888:8888 -v D:\Docker\Users\logs:/logs --name j-config -d --privileged --network my-network j-config
+
 cd ../../j-stream/target
 copy j-eureka/build-jenkins/Dockerfile j-stream/target/Dockerfile
-docker build -t j-stream-1.5.4 . && docker run -p 8088:8088 -v D:\Docker\Users\logs:/logs --name j-stream-1.5.4 -d --privileged --network my-network j-stream-1.5.4
+docker build -t j-stream-1.5.4 . && docker run -p 8686:8686 -v D:\Docker\Users\logs:/logs --name j-stream-1.5.4 -d --privileged --network my-network j-stream-1.5.4
