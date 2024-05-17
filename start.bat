e792e0138071a5c99dcffe4b8947710d05c238bb
64 <337667699@qq.com>
2024-05-17 11:40:59 +0800

    start.bat

--- start.bat
+++ start.bat
@@ -8,13 +8,13 @@ docker rmi stream.1.5.4:latest
 
 cd j-eureka
 copy build-jenkins/Dockerfile target/Dockerfile
-cd ../target
+cd target
 docker build -t j-eureka-2.7.4 . && docker run -p 8088:8088 -v D:\Docker\Users\logs:/logs --name eurekadocker.2.7.4 -d --privileged --network my-network j-eureka-2.7.4
 
 cd ../../j-config-server
 copy build-jenkins/Dockerfile target/Dockerfile
+cd target
 docker build -t j-config . && docker run -p 8888:8888 -v D:\Docker\Users\logs:/logs --name j-config -d --privileged --network my-network j-config
 
-cd ../../j-stream/target
-copy build-jenkins/Dockerfile target/Dockerfile
+cd ../../j-stream/target/target
 docker build -t j-stream-1.5.4 . && docker run -p 8686:8686 -v D:\Docker\Users\logs:/logs --name j-stream-1.5.4 -d --privileged --network my-network j-stream-1.5.4
