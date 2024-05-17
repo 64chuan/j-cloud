126bce844af223d5ed1398e705148d38b266348b
64 <337667699@qq.com>
2024-05-17 13:41:12 +0800

    start.bat

--- start.bat
+++ start.bat
@@ -1,22 +1,26 @@
-docker stop eurekadocker.2.7.4
-docker rm -f eurekadocker.2.7.4
-docker rmi j-eureka-2.7.4:latest
+docker stop eurekadocker
+docker rm -f eurekadocker
+docker rmi j-eureka:latest
 
-docker stop stream.1.5.4
-docker rm -f stream.1.5.4
-docker rmi stream.1.5.4:latest
+docker stop configdocker
+docker rm -f configdocker
+docker rmi j-config:latest
+
+docker stop streamdocker
+docker rm -f streamdocker
+docker rmi j-stream:latest
 
 cd j-eureka
 copy build-jenkins/Dockerfile target/Dockerfile
 cd target
-docker build -t j-eureka-2.7.4 . && docker run -p 8088:8088 -v D:\Docker\Users\logs:/logs --name eurekadocker.2.7.4 -d --privileged --network my-network j-eureka-2.7.4
+docker build -t j-eureka . && docker run -p 8088:8088 -v D:\Docker\Users\logs:/logs --name eurekadocker -d --privileged --network my-network j-eureka
 
 cd ../../j-config-server
 copy build-jenkins/Dockerfile target/Dockerfile
 cd target
-docker build -t j-config . && docker run -p 8888:8888 -v D:\Docker\Users\logs:/logs --name j-config -d --privileged --network my-network j-config
+docker build -t j-config . && docker run -p 8888:8888 -v D:\Docker\Users\logs:/logs --name configdocker -d --privileged --network my-network j-config
 
 cd ../../j-stream
 copy build-jenkins/Dockerfile target/Dockerfile
 cd target
-docker build -t j-stream-1.5.4 . && docker run -p 8686:8686 -v D:\Docker\Users\logs:/logs --name j-stream-1.5.4 -d --privileged --network my-network j-stream-1.5.4
+docker build -t j-stream . && docker run -p 8686:8686 -v D:\Docker\Users\logs:/logs --name streamdocker -d --privileged --network my-network j-stream
